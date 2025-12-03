910d88599b0b8b32a0208804ef9495e642ff315a
Martin <alvarezoteromartin@gmail.com>
2025-12-03 15:34:42 +0000

    Prints the command the user entered and then prints an error

--- src/main.zig
+++ src/main.zig
@@ -5,4 +5,11 @@ const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
     try stdout.print("$ ", .{});
+    var stdinBuffer: [4096]u8 = undefined; // sets a array of 4096 u8s as a buffer
+    var stdinReader = std.fs.File.stdin().readerStreaming(&stdinBuffer); //Reads from buffer into reader
+    const stdin = &stdinReader.interface; // pointer to interface in reader struct
+
+    const command = stdin.takeDelimiter('\n'); // new line as delimiter
+
+    try stdout.print("{s}: command not found", .{command.?});
 }
