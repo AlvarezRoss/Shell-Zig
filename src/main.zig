3991f4c583a704b43de88c46f18b2f44e511d991
Martin <alvarezoteromartin@gmail.com>
2025-12-03 21:24:02 +0000

    allows exit

--- src/main.zig
+++ src/main.zig
@@ -7,12 +7,15 @@ pub fn main() !void {
     var stdinBuffer: [4096]u8 = undefined; // sets a array of 4096 u8s as a buffer
     var stdinReader = std.fs.File.stdin().readerStreaming(&stdinBuffer); //Reads from buffer into reader
     const stdin = &stdinReader.interface; // pointer to interface in reader struct
+    // takes user input from keyboard
 
     while (true) {
         try stdout.print("$ ", .{});
-        const command = try stdin.takeDelimiter('\n'); // takes user input from keyboard
+        const command = try stdin.takeDelimiter('\n'); // takes user input
         // with new line as delimiter
-
+        // this if statement checks char by char if they are the same, so first argument has to be
+        // u8 not []u8
+        if (std.mem.eql(u8, command.?, "exit")) break;
         try stdout.print("{s}: command not found\n", .{command.?});
     }
 }
