9a8c73c7d0d4466a2b8d5b52062fadee7a2a9ba6
Martin <alvarezoteromartin@gmail.com>
2025-12-05 19:56:09 +0000

    added echo and type

--- src/main.zig
+++ src/main.zig
@@ -9,13 +9,45 @@ pub fn main() !void {
     const stdin = &stdinReader.interface; // pointer to interface in reader struct
     // takes user input from keyboard
 
+    var allocatorBuffer: [1024]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&allocatorBuffer);
+    const allocator = fba.allocator();
     while (true) {
         try stdout.print("$ ", .{});
-        const command = try stdin.takeDelimiter('\n'); // takes user input
+        const commandLine = try stdin.takeDelimiter('\n'); // takes user input
         // with new line as delimiter
         // this if statement checks char by char if they are the same, so first argument has to be
         // u8 not []u8
-        if (std.mem.eql(u8, command.?, "exit")) break;
-        try stdout.print("{s}: command not found\n", .{command.?});
+        if (std.mem.eql(u8, commandLine.?, "exit")) break;
+        try ParseConsoleCommand(allocator, commandLine.?);
     }
+}
+
+pub fn ParseConsoleCommand(allocator: std.mem.Allocator, command: []const u8) !void {
+    const consoleCommand = try allocator.alloc(u8, 100);
+    var index: usize = 0;
+    for (command) |char| {
+        if (char != ' ') {
+            consoleCommand[index] = char;
+            index += 1;
+        } else {
+            consoleCommand[index] = 0;
+            break;
+        }
+    }
+    if (std.mem.eql(u8, consoleCommand[0..index], "echo")) {
+        try stdout.print("{s}\n", .{command[index + 1 ..]});
+        return;
+    } else if (std.mem.eql(u8, consoleCommand[0..index], "type") and isType(command[index + 1 ..])) {
+        try stdout.print("<{s}>: is a built-in type\n", .{command[index + 1 ..]});
+    } else {
+        try stdout.print("{s}: command not found\n", .{command});
+    }
+    allocator.free(consoleCommand);
+}
+
+pub fn isType(command: []const u8) bool {
+    if (std.mem.eql(u8, command, "echo") or std.mem.eql(u8, command, "exit")) return true;
+
+    return false;
 }
